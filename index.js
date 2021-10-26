ed9b2fb37c00714f657ec6d400ab5b8a1fd01f8f
yuansu76 <85167421+yuansu76@users.noreply.github.com>
2021-10-26 20:19:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+function jsCopy1(){
+    var a=document.getElementById("content");//对象是content
+    a.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy2(){
+    var b=document.getElementById("content1");//对象是content1
+    b.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy3(){
+    var c=document.getElementById("content2");//对象是content2
+    c.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy4(){
+    var d=document.getElementById("content3");//对象是content3
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy5(){
+    var d=document.getElementById("content4");//对象是content4
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy6(){
+    var d=document.getElementById("content5");//对象是content5
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy7(){
+    var d=document.getElementById("content6");//对象是content6
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy8(){
+    var d=document.getElementById("content7");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy9(){
+    var d=document.getElementById("content8");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy10(){
+    var d=document.getElementById("content9");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy11(){
+    var d=document.getElementById("content10");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy12(){
+    var d=document.getElementById("content11");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy13(){
+    var d=document.getElementById("content12");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy14(){
+    var d=document.getElementById("content13");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy15(){
+    var d=document.getElementById("content14");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
+
+function jsCopy16(){
+    var d=document.getElementById("content15");//对象是content7
+    d.select(); //选择对象
+    document.execCommand("Copy"); //执行浏览器复制命令  
+    alert("复制成功！");
+}
